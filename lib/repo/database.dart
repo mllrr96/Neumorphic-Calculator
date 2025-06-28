93609052db4d0443d9188974d3b8d9e0220f8b5a
Mo <mrmoragdr@gmail.com>
2025-06-28 18:02:10 +0300

    Remove injector package, pump version

--- lib/repo/database.dart
+++ lib/repo/database.dart
@@ -1,7 +1,5 @@
-import 'package:injectable/injectable.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-@singleton
 class DatabaseRepository {
   DatabaseRepository(SharedPreferences sharedPreferences)
       : _preferences = sharedPreferences;
